85719c3deccfee82a4a116d7bb84ef7c7adea411
Michael Kuyper <mkuyper@mkdata.ch>
2020-10-01 18:41:49 -0400

    update to lastest Basic MAC

--- firmware/common/brd_wsms130.h
+++ firmware/common/brd_wsms130.h
@@ -1,3 +1,4 @@
+// Copyright (C) 2020-2020 Michael Kuyper. All rights reserved.
 // Copyright (C) 2016-2019 Semtech (International) AG. All rights reserved.
 //
 // This file is subject to the terms and conditions defined in file 'LICENSE',
@@ -32,9 +33,20 @@
 #define GPIO_TX         BRD_GPIO(PORT_A, 0)
 #define GPIO_RX         BRD_GPIO_EX(PORT_A, 1, BRD_GPIO_ACTIVE_LOW)
 
-// UART for debug output
-#define BRD_DBG_UART    1                          // UART1
-#define GPIO_DBG_TX     BRD_GPIO_AF(PORT_A, 9, 4)  // PA9, alternate function 4
+// Enabled USART peripherals
+#define BRD_USART       (BRD_USART1)
+
+// USART1
+#define BRD_USART1_DMA  BRD_DMA_CHANS(2,3)
+#define GPIO_USART1_TX  BRD_GPIO_AF(PORT_A, 9,  4)  // PA9,  alternate function 4
+#define GPIO_USART1_RX  BRD_GPIO_AF(PORT_A, 10, 4)  // PA10, alternate function 4
+
+// Debug USART
+#define BRD_DBG_UART    BRD_USART1_PORT
+
+// Personalization UART
+#define BRD_PERSO_UART  BRD_USART1_PORT
+#define GPIO_PERSO_DET  GPIO_USART1_RX
 
 // LED
 #define GPIO_LED0       BRD_GPIO(PORT_B, 5)
--- firmware/doorwindow/main.c
+++ firmware/doorwindow/main.c
@@ -1,3 +1,4 @@
+// Copyright (C) 2020-2020 Michael Kuyper. All rights reserved.
 // Copyright (C) 2016-2019 Semtech (International) AG. All rights reserved.
 //
 // This file is subject to the terms and conditions defined in file 'LICENSE',
@@ -124,8 +125,9 @@ void hall_irq (unsigned int* mask) {
     }
 }
 
-void app_main (osjob_t* job) {
+bool app_main (osjob_t* job) {
     state.job = job;
     hall_init(job);
     lwm_setmode(LWM_MODE_NORMAL);
+    return true;
 }
